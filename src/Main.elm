91a10c784f0021a811944f5a1ec6f87abc58c3b1
Jigar Gosar <jigar.gosar@gmail.com>
2020-02-24 16:18:46 +0530

    move from to

--- src/Main.elm
+++ src/Main.elm
@@ -3,17 +3,27 @@ module Main exposing (main)
 import Playground exposing (..)
 
 
+type alias Pt =
+    { x : Number, y : Number }
+
+
 
 -- Game Scaffold
 
 
 type alias Mem =
-    {}
+    { start : Pt
+    , end : Pt
+    , ticks : Number
+    }
 
 
 init : Mem
 init =
-    {}
+    { start = Pt -100 -100
+    , end = Pt 100 100
+    , ticks = 0
+    }
 
 
 update : Computer -> Mem -> Mem
